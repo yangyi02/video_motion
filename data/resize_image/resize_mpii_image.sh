cf2546b258ced9602d86a456cde8b6e424ddf1a1
yangyi02 <yangyi02@gmail.com>
2017-06-23 13:12:11 -0700

    remove OpticalFlowToolkit

--- data/resize_image/resize_mpii_image.sh
+++ data/resize_image/resize_mpii_image.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 INPUT_DIR=/home/yi/Downloads/mpii_human_pose_v1_sequences
-OUTPUT_DIR=/home/yi/Downloads/mpii-64
+OUTPUT_DIR=/home/yi/Downloads/mpii-240
 FILE_LIST=../mpii/mpii_file.list
-SIZE=64
+SIZE=240
 
 python create_file_list.py --input_dir=${INPUT_DIR} --output_file=${FILE_LIST}
 # python resize_image.py --input_dir=${INPUT_DIR} --output_dir=${OUTPUT_DIR} --size=${SIZE} --file_list=${FILE_LIST}
