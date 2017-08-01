30c69fa0be42798f1ce3a0144bad48715d27cfce
yangyi02 <yangyi02@gmail.com>
2017-08-01 14:17:07 -0700

    add train log

---
+++ exp005/model8/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MODEL_PATH="./model"
+if [[ ! -e $MODEL_PATH ]]; then
+    echo "$MODEL_PATH not exist!"
+    exit
+fi
+
+python main.py --test --init_model=./model/final.pth --test_epoch=1 --batch_size=2 --motion_range=4 --num_inputs=2 --num_channel=3 --test_dir=/home/yi/Downloads/mpii-test-64 --display 2>&1 | tee $MODEL_PATH/test.log
+
+cp test.sh $MODEL_PATH
